a918317ede22e96650d321264a0c87a638c4f1ba
Anuj Raman <anujramanc@gmail.com>
2023-05-12 08:11:50 +0530

    static version complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,43 @@ import { useState } from 'react'
 import './App.css'
 import FilterableProductTable from './components/FilterableProductTable'
 import SearchBar from './components/SearchBar'
-import { InputTable, SearchContainer } from './styles';
+import { Table, ProductTable, SearchContainer, HStack } from './styles';
 
 
 function App() {
 
   return (
     <>
-      <InputTable>
+      <Table>
         <FilterableProductTable>
-          <SearchContainer>
-            <SearchBar />
-          </SearchContainer>
+          <SearchBar />
         </FilterableProductTable>
-      </InputTable>
+        <ProductTable>
+          <HStack>
+            <h4>Name</h4>
+            <h4>Price</h4>
+          </HStack>
+          <h2>FRUITS</h2>
+          <HStack>
+            <p>Apple</p>
+            <p>$1</p>
+          </HStack>
+          <HStack>
+            <p>Mango</p>
+            <p>$1</p>
+          </HStack>
+          <h2>VEGETABLES</h2>
+          <HStack>
+            <p>Leafy</p>
+            <p>$4</p>
+          </HStack>
+          <HStack>
+            <p>Cabbage</p>
+            <p>$10</p>
+          </HStack>
+        </ProductTable>
+      </Table>
+
     </>
   )
 }
--- src/styles.js
+++ src/styles.js
@@ -2,15 +2,34 @@ import styled from 'styled-components'
 
 
 
-export const InputTable = styled.div`
+export const Table = styled.div`
   background-color: #fff;
   border: 1px solid #F4F4F5;
   border-radius: 6px;
-  width: 60%;
+  width: 50%;
   margin: auto;
+  padding-top: 20px;
 `
 
 export const SearchContainer = styled.div`
+  padding: 20px;
   margin: 16px auto;
   width: 100%;
+`
+
+export const ProductTable = styled.div`
+  height: 100%;
+  background-color: #F4F4F5;
+  display: block;
+  padding: 16px 0px;
+`
+
+export const HStack = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 20px;
+  width: 60%;
+  margin: auto;
 `
