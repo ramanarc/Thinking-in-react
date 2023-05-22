b3a876d1e56f0065dcc850b652978747d36a010d
Anuj Raman <anujramanc@gmail.com>
2023-05-22 13:08:20 +0530

    using state & onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
+import { MouseEvent } from "react"
 
 const ListGroup = () => {
 
   const cities = ['New York', 'San Francisco', 'Tel Aviv', 'Italy', 'Mumbai', 'Paris']
 
+  const [selectedIndex, setSelectedIndex] = useState(-1); //would go from -1 to the index of clicked item
+
   return (
     <div className='m-4'>
       <h2>List</h2>
       <ul className="list-group">
-        {cities.map((city) => <li onClick={() => console.log(city)} key={city} className="list-group-item">{city}</li>)}
+        {cities.map((city, index) => <li
+          onClick={() => setSelectedIndex(index)}
+          key={city}
+          className={selectedIndex === index ? "list-group-item active" : "list-group-item"}>
+          {city}</li>)}
       </ul>
     </div>
   )
